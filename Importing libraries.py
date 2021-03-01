c03a603e51ac886baf6fb5b317906d969e8b548d
Lavita91 <lavita.singhania@gmail.com>
2021-03-01 18:36:52 +0530

    Create Importing libraries.py

---
+++ Importing libraries.py
@@ -0,0 +1,20 @@
+#Data manipulation libraries
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+#Data visualization libraries
+import plotly.express as px
+import pandas as pd 
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+
+import matplotlib as mlt
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+#K-means clustering libraries 
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans, AffinityPropagation
